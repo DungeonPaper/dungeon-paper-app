4d924132c25f617bb6aeafe8e912d62b0f0e40f0
Chen Asraf <chen@asraf.me>
2019-03-09 09:50:46 +0200

    prettier sidebar

--- lib/main_view/sidebar.dart
+++ lib/main_view/sidebar.dart
@@ -6,73 +6,107 @@ import 'package:dungeon_paper/redux/stores/connectors.dart';
 import 'package:dungeon_paper/redux/stores/stores.dart';
 import 'package:flutter/material.dart';
 
-class Sidebar extends StatefulWidget {
-  @override
-  SidebarState createState() {
-    return new SidebarState();
-  }
-}
-
-class SidebarState extends State<Sidebar> {
-  bool expanded = false;
-
+class Sidebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DWStoreConnector<DWStore>(
       builder: (context, state) {
-        List<Widget> characterItems = [];
         DbUser user = state.user.current;
-
-        Widget title = UserAccountsDrawerHeader(
-          accountEmail: Text(user.email),
-          accountName: Text(user.displayName),
-          currentAccountPicture: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: NetworkImage(user.photoURL),
-              ),
-            ),
-          ),
-          onDetailsPressed: () {
-            setState(() {
-              expanded = !expanded;
-            });
-          },
-        );
-        Widget addNew = ListTile(
-          title: Text('+ Create Empty Character'),
-          onTap: () {
-            createNewCharacter();
-          },
-        );
-        Widget logOut = ListTile(
-          title: Text('Log out'),
-          onTap: () {
-            Navigator.pop(context);
-            requestSignOut();
-          },
-        );
-        characterItems.add(title);
-        state.characters.characters.forEach((id, character) {
-          Widget charLine = ListTile(
-            title: Text(character.displayName),
-            onTap: () {
-              dwStore.dispatch(CharacterActions.setCurrentChar(id, character));
-              Navigator.pop(context);
-            },
-          );
-          characterItems.add(charLine);
-        });
-        characterItems.add(addNew);
-        characterItems.add(logOut);
+        List<Widget> items = [
+              header(user),
+              title('Characters', context),
+            ] +
+            characterList(state.characters.characters, context) +
+            [
+              Divider(),
+              title('Application', context),
+              addNew(),
+              logOut(context),
+            ];
 
         return Drawer(
           child: ListView(
-            children: characterItems,
+            children: items,
           ),
         );
       },
     );
+  }
+
+  Widget title(String text, BuildContext context) {
+    TextStyle titleStyle = getTitleStyle(context);
+    Widget title = Padding(
+      padding: EdgeInsets.all(16).copyWith(bottom: 0),
+      child: Text(
+        text.toUpperCase(),
+        style: titleStyle,
+      ),
+    );
+    return title;
+  }
+
+  Widget logOut(BuildContext context) {
+    Widget logOut = ListTile(
+      leading: Icon(Icons.exit_to_app),
+      title: Text('Log out'),
+      onTap: doLogOut(context),
+    );
+    return logOut;
+  }
+
+  Widget addNew() {
+    Widget addNew = ListTile(
+      leading: Icon(Icons.add),
+      title: Text('Create Empty Character'),
+      onTap: addNewCharacter,
+    );
+    return addNew;
+  }
+
+  Widget header(DbUser user) {
+    Widget header = UserAccountsDrawerHeader(
+      accountEmail: Text(user.email),
+      accountName: Text(user.displayName),
+      currentAccountPicture: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(user.photoURL),
+          ),
+        ),
+      ),
+    );
+    return header;
+  }
+
+  TextStyle getTitleStyle(BuildContext context) => TextStyle(
+        color: Theme.of(context).primaryColor,
+        fontSize: 12,
+      );
+
+  List<ListTile> characterList(
+      Map<String, DbCharacter> characters, BuildContext context) {
+    return characters.keys.map((id) {
+      DbCharacter character = characters[id];
+      return ListTile(
+        leading: Icon(Icons.person),
+        title: Text(character.displayName),
+        onTap: () {
+          dwStore.dispatch(CharacterActions.setCurrentChar(id, character));
+          Navigator.pop(context);
+        },
+      );
+    }).toList();
+  }
+
+  void Function() doLogOut(BuildContext context) {
+    return () {
+      Navigator.pop(context);
+      requestSignOut();
+    };
+  }
+
+  void addNewCharacter() {
+    createNewCharacter();
   }
 }
