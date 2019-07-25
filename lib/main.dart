c5b75b0453bfc497dfd77122ae9649e6b5de95ff
Chen Asraf <chenasraf@users.noreply.github.com>
2019-07-25 21:16:15 +0300

    added error reporting

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,51 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dungeon_paper/main_view/main_view.dart';
 import 'package:dungeon_paper/redux/actions.dart';
 import 'package:dungeon_paper/redux/stores/stores.dart';
+import 'package:dungeon_paper/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:screen/screen.dart';
+import 'package:sentry/sentry.dart';
 
 void main() async {
+  // Error reporting
+  var secrets = await loadSecrets();
+  SentryClient _sentry = SentryClient(dsn: secrets['SENTRY_DSN']);
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isInDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  Future<void> reportError(dynamic error, dynamic stackTrace) async {
+    print('Caught error: $error');
+    if (isInDebugMode) {
+      print(stackTrace);
+      return;
+    } else {
+      _sentry.captureException(
+        exception: error,
+        stackTrace: stackTrace,
+      );
+    }
+  }
+
+  // general setup
   Firestore firestore = Firestore.instance;
   await firestore.settings(timestampsInSnapshotsEnabled: true);
   Screen.keepOn(true);
-  runApp(DungeonPaper());
-  dwStore.dispatch(AppInit());
+
+  runZoned(() async {
+    runApp(DungeonPaper());
+    dwStore.dispatch(AppInit());
+  }, onError: (err, trace) {
+    reportError(err, trace);
+  });
 }
 
 class DungeonPaper extends StatelessWidget {
