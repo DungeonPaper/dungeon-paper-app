c0e46f23c75ab0805edcbfc2e39e1f7588474930
Chen Asraf <chenasraf@users.noreply.github.com>
2019-09-21 20:51:06 +0300

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 push=0
+install=0
 while test $# -gt 0; do
   case "$1" in
     -p|--push) push=1; shift;;
+    -i|--install) install=1; shift;;
     *) version=$1; shift;;
   esac
 done
 
 sep=' '
 if [[ $version ]]; then echo "Version: $version"; fi
-if [[ $push ]]; then sep=" & Pushing "; fi
+if [[ $push ]]; then sep="$sep& Pushing "; fi
+if [[ $install ]]; then sep="$sep& Installing "; fi
 echo "Building${sep}APK"
 
 platforms="android-arm,android-arm64"
@@ -18,5 +21,11 @@ apk_file="build/app/outputs/apk/release/app-arm64-v8a-release.apk"
 flutter build appbundle --target-platform ${platforms}
 flutter build apk --target-platform ${platforms} --split-per-abi
 if [[ $push ]]; then
+  echo "Pushing ${apk_file} to /sdcard/Download/dungeon-paper-${version}.apk"
   adb push ${apk_file} /sdcard/Download/dungeon-paper-${version}.apk
+fi
+
+if [[ $install ]]; then
+  echo "Installing ${apk_file}"
+  adb install -r ${apk_file}
 fi
