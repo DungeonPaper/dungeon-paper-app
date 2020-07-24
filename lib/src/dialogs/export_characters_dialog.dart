f0ff34ec431fa23daa0fe53dcb02d1c6b0433a78
Chen Asraf <chen@asraf.me>
2020-07-24 22:28:19 +0300

    cleanups

--- lib/src/dialogs/export_characters_dialog.dart
+++ lib/src/dialogs/export_characters_dialog.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
-import 'package:pedantic/pedantic.dart';
 
 class ExportCharactersDialog extends StatefulWidget {
   @override
