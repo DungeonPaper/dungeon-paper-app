3fa2af95700167a12b9740d3f6764915e8258802
Chen Asraf <chen@asraf.me>
2020-07-24 22:40:16 +0300

    loading container

---
+++ lib/src/flutter_utils/loading_container.dart
@@ -0,0 +1,26 @@
+import 'package:dungeon_paper/src/flutter_utils/widget_utils.dart';
+import 'package:flutter/material.dart';
+
+class LoadingContainer extends StatelessWidget {
+  final bool loading;
+  final Widget Function() builder;
+  final Widget child;
+  final Widget loader;
+
+  const LoadingContainer({
+    Key key,
+    @required this.loading,
+    this.builder,
+    this.child,
+    this.loader,
+  })  : assert(builder != null || child != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (loading == true) {
+      return loader ?? Center(child: PageLoader());
+    }
+    return child ?? builder?.call();
+  }
+}
