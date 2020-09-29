4610236f421c4262984b5f616697e6c1cd0ea454
Chen Asraf <chen@asraf.me>
2020-09-29 19:09:17 +0300

    improve auth flow

--- lib/src/redux/shared_preferences/shared_prefs_middleware.dart
+++ lib/src/redux/shared_preferences/shared_prefs_middleware.dart
@@ -12,9 +12,9 @@ void sharedPrefsMiddleware(Store<DWStore> store, action, NextDispatcher next) {
 
   if (action is SetPrefs) {
     withPrefs((prefs) {
-      for (var el in action.prefs.credentials.serialize().entries) {
-        prefs.setString(el.key, el.value);
-      }
+      // for (var el in action.prefs.credentials.serialize().entries) {
+      //   prefs.setString(el.key, el.value);
+      // }
       prefs.setString(keyMap[SharedPrefKeys.UserEmail], action.prefs.user.id);
       prefs.setString(keyMap[SharedPrefKeys.UserId], action.prefs.user.email);
       prefs.setString(keyMap[SharedPrefKeys.CharacterId],
@@ -22,15 +22,15 @@ void sharedPrefsMiddleware(Store<DWStore> store, action, NextDispatcher next) {
     });
   }
 
-  if (action is Credentials) {
-    withPrefs((prefs) {
-      prefs.setString(keyMap[SharedPrefKeys.SignInProvider],
-          action.providerCredentials.providerId);
-      for (var el in action.serialize().entries) {
-        prefs.setString(el.key, el.value);
-      }
-    });
-  }
+  // if (action is Credentials) {
+  //   withPrefs((prefs) {
+  //     prefs.setString(keyMap[SharedPrefKeys.SignInProvider],
+  //         action.providerCredentials.providerId);
+  //     for (var el in action.serialize().entries) {
+  //       prefs.setString(el.key, el.value);
+  //     }
+  //   });
+  // }
 
   if (action is SetCurrentChar) {
     withPrefs((prefs) {
@@ -49,16 +49,16 @@ void sharedPrefsMiddleware(Store<DWStore> store, action, NextDispatcher next) {
     withPrefs((prefs) {
       prefs.setString(keyMap[SharedPrefKeys.UserId], action.user.documentID);
       prefs.setString(keyMap[SharedPrefKeys.UserEmail], action.user.email);
-      for (var el in action.credentials.serialize().entries) {
-        prefs.setString(el.key, el.value);
-      }
+      // for (var el in action.credentials.serialize().entries) {
+      //   prefs.setString(el.key, el.value);
+      // }
     });
   }
 
   if (action is Logout) {
     withPrefs((prefs) {
-      prefs.remove(keyMap[SharedPrefKeys.IdToken]);
-      prefs.remove(keyMap[SharedPrefKeys.AccessToken]);
+      // prefs.remove(keyMap[SharedPrefKeys.IdToken]);
+      // prefs.remove(keyMap[SharedPrefKeys.AccessToken]);
       prefs.remove(keyMap[SharedPrefKeys.UserId]);
       prefs.remove(keyMap[SharedPrefKeys.UserEmail]);
       prefs.remove(keyMap[SharedPrefKeys.CharacterId]);
