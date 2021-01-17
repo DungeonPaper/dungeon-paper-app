6d22243c6d8438e8ad35f9c941ceca4f86d86707
Chen Asraf <chen@asraf.me>
2021-01-17 12:12:33 +0200

    feat: move template preview/explanation

--- lib/db/models/move_templates.dart
+++ lib/db/models/move_templates.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/foundation.dart';
 
 class MoveTemplate {
-  final String label;
+  final String shortLabel;
+  final String longLabel;
   final String text;
   final String help;
 
   const MoveTemplate({
-    @required this.label,
+    @required this.shortLabel,
+    @required this.longLabel,
     @required this.text,
     @required this.help,
   });
--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@ import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -20,6 +21,7 @@ void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
