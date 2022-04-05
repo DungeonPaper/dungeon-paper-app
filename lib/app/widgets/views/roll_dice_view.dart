8ccd9c7443e7a8d75aa65f2eae87930c0953ed7a
Chen Asraf <chenasrafil@gmail.com>
2022-04-06 02:47:47 +0300

    major dice roll dialog improvements

--- lib/app/widgets/views/roll_dice_view.dart
+++ lib/app/widgets/views/roll_dice_view.dart
@@ -1,12 +1,10 @@
-import 'dart:math';
+import 'dart:async';
+import 'dart:ui';
 
 import 'package:dungeon_paper/app/data/services/character_service.dart';
 import 'package:dungeon_paper/app/model_utils/dice_utils.dart';
 import 'package:dungeon_paper/app/themes/colors.dart';
 import 'package:dungeon_paper/app/widgets/atoms/dice_icon.dart';
-import 'package:dungeon_paper/app/widgets/atoms/svg_icon.dart';
-import 'package:dungeon_paper/core/dw_icons.dart';
-import 'package:dungeon_paper/core/utils/grid_point.dart';
 import 'package:dungeon_paper/core/utils/list_utils.dart';
 import 'package:dungeon_paper/core/utils/math_utils.dart';
 import 'package:flutter/material.dart';
@@ -25,164 +23,183 @@ class RollDiceView extends StatefulWidget {
   State<RollDiceView> createState() => _RollDiceViewState();
 }
 
-class _RollDiceViewState extends State<RollDiceView> with SingleTickerProviderStateMixin {
+class _RollDiceViewState extends State<RollDiceView> with TickerProviderStateMixin {
   final diceSize = 56.0;
   final diceSpacing = 24.0;
   late AnimationStatus rollStatus;
-
-  late Animation<double> yAnim;
-  late Animation<double> rotateAnim;
-  late Animation<double> opacityAnim;
-  late AnimationController animController;
-
-  late List<List<_AnimSet>> anims;
-
+  late List<List<_AnimSet>> animations;
   late List<dw.Dice> dice;
+  late List<dw.Dice> withoutModDice;
   var results = <dw.DiceRoll>[];
+
   CharacterService get charService => Get.find();
+  int get totalResult => results.fold(0, (previousValue, element) => previousValue + element.total);
+  List<dw.Dice> get flat => dw.Dice.flatten(dice);
 
   @override
   void initState() {
     super.initState();
+    withoutModDice = widget.dice;
     dice = _applyMods(widget.dice);
-    roll();
+    rollStatus = AnimationStatus.dismissed;
+    _roll();
     _setupAnimations();
-  }
-
-  void _setupAnimations() {
-    final forwardTween = Tween<double>(begin: 0, end: 1);
-    final reverseTween = Tween<double>(begin: 1, end: 0);
-
-    animController = AnimationController(vsync: this, duration: const Duration(milliseconds: 2500));
-    rotateAnim = reverseTween.animate(CurvedAnimation(
-      parent: animController,
-      curve: const Interval(0, 0.75, curve: Curves.easeOutQuad),
-    ));
-    yAnim = reverseTween.animate(CurvedAnimation(
-      parent: animController,
-      curve: const Interval(0, 0.75, curve: Curves.fastOutSlowIn),
-    ));
-    opacityAnim = forwardTween.animate(CurvedAnimation(
-      parent: animController,
-      curve: const Interval(0.75, 1, curve: Curves.easeOutCubic),
-    ));
-    yAnim.addListener(_setEmptyState);
-    yAnim.addStatusListener(_setStatus);
-    animController.forward();
-  }
-
-  void _setEmptyState() {
-    setState(() {});
-  }
-
-  void _setStatus(status) {
-    setState(() {
-      rollStatus = status;
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    animController.dispose();
-    yAnim.removeListener(_setEmptyState);
-    yAnim.removeStatusListener(_setStatus);
   }
 
   @override
   Widget build(BuildContext context) {
-    var bgColor = Colors.black.withOpacity(0.85);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(rollStatus == AnimationStatus.completed
-            ? 'Result: $totalResult'
-            : 'Rolling ${flat.length} dice'),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.white,
-      ),
-      backgroundColor: bgColor,
-      body: LayoutBuilder(builder: (context, constraints) {
-        final maxHeight = MediaQuery.of(context).size.height;
+    final bgColor = Colors.black.withOpacity(0.85);
+    final textTheme = Theme.of(context).textTheme;
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+      // blendMode: BlendMode.darken,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(rollStatus == AnimationStatus.completed
+              ? 'Result: $totalResult'
+              : 'Rolling ${flat.length} dice'),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.white,
+        ),
+        backgroundColor: bgColor,
+        body: LayoutBuilder(builder: (context, constraints) {
+          final maxHeight = MediaQuery.of(context).size.height;
 
-        return Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                runSpacing: diceSpacing,
-                spacing: diceSpacing,
-                children: [
-                  for (final dice in enumerate(flat))
-                    Transform.translate(
-                      offset: Offset(
-                        0,
-                        yAnim.value * maxHeight,
-                      ),
-                      child: Transform.rotate(
-                        angle: degToRad(360 * 3 * rotateAnim.value),
-                        child: Stack(
-                          children: [
-                            DiceIcon.from(
-                              dice.value,
-                              size: diceSize,
-                              color: Colors.white,
-                            ),
-                            Positioned.fill(
-                              child: Opacity(
-                                opacity: opacityAnim.value,
-                                child: Center(
+          return Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    for (final group in enumerate(dice))
+                      Padding(
+                        padding: group.index != dice.length - 1
+                            ? const EdgeInsets.only(bottom: 24)
+                            : EdgeInsets.zero,
+                        child: Material(
+                          color: Colors.black
+                              .withOpacity(0.4 * animations[group.index].first.opacity.value),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Opacity(
+                                  opacity: animations[group.index].first.opacity.value,
                                   child: Transform.translate(
-                                    offset: DiceUtils.iconCenterOffset(dice.value),
-                                    child: SizedBox(
-                                      width: 30,
-                                      height: 30,
-                                      child: Material(
-                                        type: MaterialType.circle,
-                                        color: Colors.black,
-                                        child: Center(
-                                          child: Text(
-                                            results[dice.index].total.toString(),
-                                            style: const TextStyle(
-                                              color: Colors.white,
-                                            ),
-                                            textScaleFactor: 1.5,
-                                          ),
+                                    offset: Offset(
+                                      0,
+                                      animations[group.index].first.y.value * maxHeight,
+                                    ),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Total: ${results[group.index].total}',
+                                          style: textTheme.headline6!.copyWith(color: Colors.white),
                                         ),
-                                      ),
+                                        Text(
+                                          'Dice: ' +
+                                              (withoutModDice[group.index]).toString() +
+                                              ' | '
+                                                  'Modifier: ' +
+                                              (group.value.modifierWithSign.isEmpty
+                                                  ? '+0'
+                                                  : group.value.modifierWithSign),
+                                          style: textTheme.bodyText2!
+                                              .copyWith(color: Colors.white.withOpacity(0.75)),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
-                              ),
+                                const SizedBox(height: 8),
+                                Wrap(
+                                  alignment: WrapAlignment.start,
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  runSpacing: diceSpacing,
+                                  spacing: diceSpacing,
+                                  children: [
+                                    for (final d in enumerate(dw.Dice.flatten([group.value])))
+                                      Transform.translate(
+                                        offset: Offset(
+                                          0,
+                                          animations[group.index][d.index].y.value * maxHeight,
+                                        ),
+                                        child: Transform.rotate(
+                                          angle: degToRad(360 *
+                                              3 *
+                                              animations[group.index][d.index].angle.value),
+                                          child: Stack(
+                                            children: [
+                                              DiceIcon.from(
+                                                group.value,
+                                                size: diceSize,
+                                                color: Colors.white,
+                                              ),
+                                              Positioned.fill(
+                                                child: Opacity(
+                                                  opacity: animations[group.index][d.index]
+                                                      .opacity
+                                                      .value,
+                                                  child: Center(
+                                                    child: Transform.translate(
+                                                      offset:
+                                                          DiceUtils.iconCenterOffset(group.value),
+                                                      child: SizedBox(
+                                                        width: 30,
+                                                        height: 30,
+                                                        child: Material(
+                                                          type: MaterialType.circle,
+                                                          color: Colors.black,
+                                                          child: Center(
+                                                            child: Text(
+                                                              results[group.index]
+                                                                  .results[d.index]
+                                                                  .toString(),
+                                                              style: const TextStyle(
+                                                                color: Colors.white,
+                                                              ),
+                                                              textScaleFactor: 1.5,
+                                                            ),
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      }),
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: DwColors.success,
-        label: const Text('Roll again'),
-        icon: const Icon(Icons.refresh),
-        onPressed: _reRoll,
+          );
+        }),
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: DwColors.success,
+          label: const Text('Roll again'),
+          icon: const Icon(Icons.refresh),
+          onPressed: _reRoll,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-  }
-
-  double getTop(int index, int columnCount) {
-    return GridPoint.fromIndex(index, columnCount).row * (diceSize + diceSpacing);
-  }
-
-  double getLeft(int index, int columnCount) {
-    return GridPoint.fromIndex(index, columnCount).col * (diceSize + diceSpacing);
   }
 
   List<dw.Dice> _applyMods(List<dw.Dice> dice) {
@@ -197,26 +214,85 @@ class _RollDiceViewState extends State<RollDiceView> with SingleTickerProviderSt
         .toList();
   }
 
-  int get totalResult => results.fold(0, (previousValue, element) => previousValue + element.total);
-
-  List<dw.Dice> get flat => dw.Dice.flatten(dice);
-
-  roll() {
-    results = dw.DiceRoll.rollMany(flat);
+  _roll() {
+    results = dw.DiceRoll.rollMany(dice);
   }
 
-  _reRoll() {
-    animController.reset();
-    roll();
-    animController.forward();
+  _reRoll() async {
+    // play back quickly
+    await _walkAnimations((animation, groupIndex, animIndex) {
+      animation.controller.duration = const Duration(milliseconds: 1000);
+      animation.controller.reverse();
+    });
+    // wait for animation
+    await Future.delayed(Duration(milliseconds: 800 + (animations.length * 30)));
+
+    // revert duration change
+    await _walkAnimations((animation, groupIndex, animIndex) {
+      animation.controller.duration = const Duration(milliseconds: 2500);
+    });
+
+    _roll();
+    _runAnimations();
+  }
+
+  void _runAnimations() async {
+    _walkAnimations((anim, groupIndex, animIndex) async {
+      await Future.delayed(Duration(milliseconds: (groupIndex * 30) + (animIndex * 15)));
+      anim.controller.forward();
+    });
+  }
+
+  FutureOr<void> _walkAnimations(
+      FutureOr<void> Function(_AnimSet animation, int groupIndex, int animIndex) cb) async {
+    for (final group in enumerate(animations)) {
+      for (final anim in enumerate(group.value)) {
+        await cb(anim.value, group.index, anim.index);
+      }
+    }
+  }
+
+  void _setupAnimations() async {
+    animations = List.generate(
+      dice.length,
+      (index) => List.generate(
+        dice[index].amount,
+        (index) => _AnimSet(vsync: this),
+      ),
+    );
+    animations.first.first.controller.addListener(_setEmptyState);
+    animations.last.last.controller.addListener(_setEmptyState);
+    animations.last.last.controller.addStatusListener(_setStatus);
+    _runAnimations();
+    // animController.forward();
+  }
+
+  void _setEmptyState() {
+    setState(() {});
+  }
+
+  void _setStatus(status) {
+    setState(() {
+      rollStatus = status;
+    });
+  }
+
+  @override
+  void dispose() {
+    for (final group in animations) {
+      for (final animation in group) {
+        animation.dispose();
+      }
+    }
+    super.dispose();
   }
 }
 
 class _AnimSet {
+  late final AnimationController controller;
   late final Animation<double> y;
   late final Animation<double> angle;
   late final Animation<double> opacity;
-  late final AnimationController controller;
 
   _AnimSet({
     required TickerProvider vsync,
@@ -241,5 +317,9 @@ class _AnimSet {
       parent: controller,
       curve: const Interval(0.75, 1, curve: Curves.easeOutCubic),
     ));
+  }
+
+  dispose() {
+    controller.dispose();
   }
 }
