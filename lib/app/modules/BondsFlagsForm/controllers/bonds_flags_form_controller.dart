d486e76b7f03de7ac0d969a069081b429e90e106
Chen Asraf <chenasrafil@gmail.com>
2022-05-17 03:17:00 +0300

    confirm exit in bonds/flags form

--- lib/app/modules/BondsFlagsForm/controllers/bonds_flags_form_controller.dart
+++ lib/app/modules/BondsFlagsForm/controllers/bonds_flags_form_controller.dart
@@ -10,15 +10,20 @@ class BondsFlagsFormController extends GetxController {
   final bondsDesc = <TextEditingController>[].obs;
   final flagsDesc = <TextEditingController>[].obs;
   late final void Function(List<SessionMark> bonds, List<SessionMark> flags) onChanged;
+  final dirty = false.obs;
 
   @override
   void onReady() {
     super.onReady();
     final BondsFlagsFormArguments args = Get.arguments;
     bonds.value = args.bonds;
-    bondsDesc.value = args.bonds.map((e) => TextEditingController(text: e.description)).toList();
+    bondsDesc.value = args.bonds
+        .map((e) => TextEditingController(text: e.description)..addListener(_setDirty))
+        .toList();
     flags.value = args.flags;
-    flagsDesc.value = args.flags.map((e) => TextEditingController(text: e.description)).toList();
+    flagsDesc.value = args.flags
+        .map((e) => TextEditingController(text: e.description)..addListener(_setDirty))
+        .toList();
     onChanged = args.onChanged;
   }
 
@@ -28,11 +33,12 @@ class BondsFlagsFormController extends GetxController {
       description: '',
       completed: false,
     ));
-    bondsDesc.add(TextEditingController());
+    bondsDesc.add(TextEditingController()..addListener(_setDirty));
   }
 
   void removeBond(int index) {
     bonds.removeAt(index);
+    bondsDesc[index].removeListener(_setDirty);
     bondsDesc.removeAt(index);
   }
 
@@ -42,12 +48,21 @@ class BondsFlagsFormController extends GetxController {
       description: '',
       completed: false,
     ));
-    flagsDesc.add(TextEditingController());
+    flagsDesc.add(TextEditingController()..addListener(_setDirty));
   }
 
   void removeFlag(int index) {
     flags.removeAt(index);
+    flagsDesc[index].removeListener(_setDirty);
     flagsDesc.removeAt(index);
+  }
+
+  @override
+  void onClose() {
+    for (final ctrl in [...bondsDesc, ...flagsDesc]) {
+      ctrl.removeListener(_setDirty);
+    }
+    super.onClose();
   }
 
   void save() {
@@ -62,6 +77,12 @@ class BondsFlagsFormController extends GetxController {
 
     onChanged(newBonds, newFlags);
     Get.back();
+  }
+
+  void _setDirty() {
+    if (!dirty.value) {
+      dirty.value = true;
+    }
   }
 }
 
--- lib/app/modules/BondsFlagsForm/views/bonds_flags_form_view.dart
+++ lib/app/modules/BondsFlagsForm/views/bonds_flags_form_view.dart
@@ -1,4 +1,5 @@
 import 'package:dungeon_paper/app/widgets/atoms/advanced_floating_action_button.dart';
+import 'package:dungeon_paper/app/widgets/atoms/confirm_exit_view.dart';
 import 'package:dungeon_paper/core/utils/list_utils.dart';
 import 'package:dungeon_paper/generated/l10n.dart';
 import 'package:flutter/material.dart';
@@ -14,65 +15,68 @@ class BondsFlagsFormView extends GetView<BondsFlagsFormController> {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return Obx(
-      () => Scaffold(
-        appBar: AppBar(
-          title: Text(S.current.characterBondsFlagsDialogTitle),
-          centerTitle: true,
-        ),
-        floatingActionButton: AdvancedFloatingActionButton.extended(
-          onPressed: controller.save,
-          icon: const Icon(Icons.save),
-          label: Text(S.current.save),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(16),
-          children: [
-            // TODO intl
-            Text('Bonds', style: textTheme.headline5),
-            for (final bond in enumerate(controller.bondsDesc))
-              ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                trailing: IconButton(
-                  icon: const Icon(Icons.remove),
-                  onPressed: () => controller.removeBond(bond.index),
+      () => ConfirmExitView(
+        dirty: controller.dirty.value,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(S.current.characterBondsFlagsDialogTitle),
+            centerTitle: true,
+          ),
+          floatingActionButton: AdvancedFloatingActionButton.extended(
+            onPressed: controller.save,
+            icon: const Icon(Icons.save),
+            label: Text(S.current.save),
+          ),
+          body: ListView(
+            padding: const EdgeInsets.all(16),
+            children: [
+              // TODO intl
+              Text('Bonds', style: textTheme.headline5),
+              for (final bond in enumerate(controller.bondsDesc))
+                ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () => controller.removeBond(bond.index),
+                  ),
+                  title: TextField(
+                    controller: bond.value,
+                    textCapitalization: TextCapitalization.sentences,
+                    minLines: 1,
+                    maxLines: 5,
+                  ),
                 ),
-                title: TextField(
-                  controller: bond.value,
-                  textCapitalization: TextCapitalization.sentences,
-                  minLines: 1,
-                  maxLines: 5,
-                ),
+              OutlinedButton.icon(
+                onPressed: () => controller.addBond(),
+                label: const Text('Create Bond'),
+                icon: const Icon(Icons.add),
               ),
-            OutlinedButton.icon(
-              onPressed: () => controller.addBond(),
-              label: const Text('Create Bond'),
-              icon: const Icon(Icons.add),
-            ),
 
-            const Divider(height: 24),
+              const Divider(height: 24),
 
-            // TODO intl
-            Text('Flags', style: textTheme.headline5),
-            for (final flag in enumerate(controller.flagsDesc))
-              ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                trailing: IconButton(
-                  icon: const Icon(Icons.remove),
-                  onPressed: () => controller.removeFlag(flag.index),
+              // TODO intl
+              Text('Flags', style: textTheme.headline5),
+              for (final flag in enumerate(controller.flagsDesc))
+                ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () => controller.removeFlag(flag.index),
+                  ),
+                  title: TextField(
+                    controller: flag.value,
+                    textCapitalization: TextCapitalization.sentences,
+                    minLines: 1,
+                    maxLines: 5,
+                  ),
                 ),
-                title: TextField(
-                  controller: flag.value,
-                  textCapitalization: TextCapitalization.sentences,
-                  minLines: 1,
-                  maxLines: 5,
-                ),
+              OutlinedButton.icon(
+                onPressed: () => controller.addFlag(),
+                label: const Text('Create Flag'),
+                icon: const Icon(Icons.add),
               ),
-            OutlinedButton.icon(
-              onPressed: () => controller.addFlag(),
-              label: const Text('Create Flag'),
-              icon: const Icon(Icons.add),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
