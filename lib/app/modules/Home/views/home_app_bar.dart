b91e6f93aa3e6f31242adfd083bc0931a448c88b
Chen Asraf <chenasrafil@gmail.com>
2022-08-17 11:16:07 +0300

    build: add version bump script

--- lib/app/modules/Home/views/home_app_bar.dart
+++ lib/app/modules/Home/views/home_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:dungeon_paper/app/data/services/loading_service.dart';
 import 'package:dungeon_paper/app/data/services/user_service.dart';
+import 'package:dungeon_paper/app/modules/Migration/controllers/migration_controller.dart';
 import 'package:dungeon_paper/app/routes/app_pages.dart';
 import 'package:dungeon_paper/app/widgets/atoms/user_menu.dart';
 import 'package:dungeon_paper/generated/l10n.dart';
@@ -25,7 +26,10 @@ class HomeAppBar extends StatelessWidget
           if (user.flags['su'] == true)
             IconButton(
               icon: const Icon(Icons.bug_report),
-              onPressed: () => Get.toNamed(Routes.migration),
+              onPressed: () => Get.toNamed(
+                Routes.migration,
+                arguments: MigrationArguments(email: user.email),
+              ),
             ),
           const UserMenu(),
         ],
