ec4d2a302233d65537916cd91fc759de7e049404
magnusanderson-wk <magnus.anderson@workiva.com>
2023-11-29 14:08:18 -0600

    Delete many files

--- lib/src/dialogs/dialogs.dart
+++
@@ -1,1 +0,0 @@
-enum DialogMode { edit, create }
--- lib/src/utils/types.dart
+++
@@ -1,4 +0,0 @@
-typedef CallbackDelegate<T, R> = R Function(T obj);
-typedef VoidCallbackDelegate<T> = void Function(T obj);
-typedef EmptyCallbackDelegate<R> = R Function();
-typedef VoidEmptyCallbackDelegate = void Function();
