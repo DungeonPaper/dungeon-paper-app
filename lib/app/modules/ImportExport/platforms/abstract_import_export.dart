3e1a4dda7e222280f4ddf1425aa628192c4a03f4
magnusanderson-wk <magnus.anderson@workiva.com>
2023-12-01 17:35:38 -0600

    bang!

--- lib/app/modules/ImportExport/platforms/abstract_import_export.dart
+++ lib/app/modules/ImportExport/platforms/abstract_import_export.dart
@@ -3,3 +3,10 @@ import 'dart:typed_data';
 abstract class AbstractExporter {
   void export(Uint8List data, String filename);
 }
+
+class Exporter extends AbstractExporter {
+  @override
+  void export(Uint8List data, String filename) {
+    throw UnimplementedError('Unsupported platform is unsupported');
+  }
+}
