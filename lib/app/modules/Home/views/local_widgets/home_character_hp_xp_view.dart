5241c48e7c965e6c841957d82d862babf1428020
magnusanderson-wk <magnus.anderson@workiva.com>
2023-12-18 09:23:40 -0600

    move +1 button

--- lib/app/modules/Home/views/local_widgets/home_character_hp_xp_view.dart
+++ lib/app/modules/Home/views/local_widgets/home_character_hp_xp_view.dart
@@ -1,48 +1,43 @@
+
+import 'package:dungeon_paper/app/data/services/character_service.dart';
 import 'package:dungeon_paper/app/widgets/atoms/xp_bar.dart';
 import 'package:dungeon_paper/app/widgets/atoms/hp_bar.dart';
-import 'package:dungeon_paper/app/widgets/chips/primary_chip.dart';
 import 'package:dungeon_paper/app/widgets/dialogs/xp_dialog.dart';
 import 'package:dungeon_paper/app/widgets/dialogs/hp_dialog.dart';
-import 'package:dungeon_paper/core/dw_icons.dart';
 import 'package:flutter/material.dart';
 
-class HomeCharacterHpExpView extends StatelessWidget {
+import 'package:get/get.dart';
+
+class HomeCharacterHpExpView extends GetView<CharacterService> {
   const HomeCharacterHpExpView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: InkWell(
-            splashColor: Theme.of(context).splashColor,
-            borderRadius: BorderRadius.circular(10),
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => const HPDialog(),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(4),
-              child: HpBar(),
-            ),
+  Widget build(BuildContext context) => Row(
+    children: [
+      Expanded(
+        child: InkWell(
+          splashColor: Theme.of(context).splashColor,
+          borderRadius: BorderRadius.circular(10),
+          onTap: () => Get.dialog(const HPDialog()),
+          child: const Padding(
+            padding: EdgeInsets.all(4),
+            child: HpBar(),
           ),
         ),
-        const SizedBox(width: 16),
-        Expanded(
-          child: InkWell(
-            splashColor: Theme.of(context).splashColor,
-            borderRadius: BorderRadius.circular(10),
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => const EXPDialog(),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(4),
-              child: ExpBar(),
-            ),
+      ),
+      const SizedBox(width: 16),
+      Expanded(
+        child: InkWell(
+          splashColor: Theme.of(context).splashColor,
+          borderRadius: BorderRadius.circular(10),
+          onTap: () => Get.dialog(const EXPDialog()),
+          child: const Padding(
+            padding: EdgeInsets.all(4),
+            child: ExpBar(showPlusOneButton: true)
           ),
         ),
-      ],
-    );
-  }
+      ),
+    ],
+  );
 }
+
--- lib/app/widgets/atoms/xp_bar.dart
+++ lib/app/widgets/atoms/xp_bar.dart
@@ -1,43 +1,69 @@
-import 'package:dungeon_paper/app/data/services/character_provider.dart';
+import 'package:dungeon_paper/app/data/services/character_service.dart';
 import 'package:dungeon_paper/app/widgets/atoms/buffer_progress_bar.dart';
+import 'package:dungeon_paper/app/widgets/chips/primary_chip.dart';
 import 'package:dungeon_paper/i18n.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class ExpBar extends StatelessWidget {
+class ExpBar extends StatelessWidget with CharacterServiceMixin {
   const ExpBar({
     super.key,
     this.currentXp,
     this.maxXp,
     this.pendingXp,
+    this.showPlusOneButton = false,
   });
 
   final int? currentXp;
   final int? maxXp;
   final int? pendingXp;
+  final bool showPlusOneButton;
 
   @override
   Widget build(BuildContext context) {
-    return CharacterProvider.consumer(
-      (context, controller, _) {
-        final char = controller.maybeCurrent;
-        final maxValue = maxXp ?? char?.maxXp;
-        final curValue = currentXp ?? char?.currentXp ?? 0;
+    return Obx(
+      () {
+        final maybeCharacter = maybeChar;
 
-        final curPercent = maxValue != null ? curValue / maxValue : 0.0;
-        final curPending = char != null ? pendingXp ?? char.pendingXp : 0;
-        final curBuffer = char != null ? curValue + (curPending) : 0;
-        final curBufferPercent = curBuffer / (maxValue ?? double.infinity);
+        final curValue = currentXp ?? maybeCharacter?.currentXp ?? 0;
+        final curPending = pendingXp ?? maybeCharacter?.pendingXp ?? 0;
+        final maxValueNullable = maxXp ?? maybeCharacter?.maxXp;
+        final maxValue = maxValueNullable ?? 1;
+        final maxValueString = maxValueNullable?.toString() ?? '-';
+
+        final curPercent = curValue / maxValue;
+        final curBuffer = curValue + (curPending);
+        final curBufferPercent = curBuffer / maxValue;
 
         return Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            BufferProgressBar(
+            Row(
+              children: [
+              Expanded(
+              child: BufferProgressBar(
               value: curPercent,
               bufferValue: curBufferPercent,
               bufferColor: const Color.fromARGB(255, 117, 188, 251),
               height: 17.5,
               color: const Color(0xff1e88e5),
               backgroundColor: Colors.blue[100],
+            ),
+              ),
+              if (showPlusOneButton)
+                PrimaryChip(
+                  label: '+1',
+                  onPressed: () {
+                    charService.updateCharacter(
+                      char.copyWith(
+                        stats: char.stats.copyWith(
+                          currentXp: 1 + (char.stats.currentXp),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
             const SizedBox(height: 4),
             Row(
@@ -49,7 +75,7 @@ class ExpBar extends StatelessWidget {
                   curValue.toString(),
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-                Text('/${maxValue?.toString() ?? '-'}'),
+                Text('/$maxValueString'),
                 if (curPending > 0)
                   Text(
                     '(+$curPending)',
