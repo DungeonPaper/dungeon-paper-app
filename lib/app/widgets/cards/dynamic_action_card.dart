36c65d3be3dce9569767428c5e732526eb4d9e25
magnusanderson-wk <magnus.anderson@workiva.com>
2024-02-19 17:20:56 -0600

    Add golden stars

--- lib/app/widgets/cards/dynamic_action_card.dart
+++ lib/app/widgets/cards/dynamic_action_card.dart
@@ -96,7 +96,7 @@ class _DynamicActionCardState extends State<DynamicActionCard> {
                 color: Theme.of(context).colorScheme.onSurface.withOpacity(0.3),
               ),
               child: widget.starred
-                  ? widget.starredIcon ?? const Icon(Icons.star_rounded)
+                  ? widget.starredIcon ?? const Icon(Icons.star_rounded, color: Color.fromRGBO(255, 215, 0, 1))
                   : widget.unstarredIcon ?? const Icon(Icons.star_border_rounded),
             ),
             onPressed: () => widget.onStarChanged?.call(!widget.starred),
